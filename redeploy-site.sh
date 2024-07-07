8392dc69fc6840abd5b666872467842014800653
audgeviolin07 <audgeviolin07@gmail.com>
2024-07-07 01:21:25 -0700

    bash

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tmux kill-server
-cd /audreychenportfolio
+cd ~/audreychenportfolio
 git fetch && git reset origin/main --hard
-source venv/bin/activate
+source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d -s my_flask_app "cd /audreychenportfolio && source venv/bin/activate && flask run --host=0.0.0.0"
+tmux new-session -d -s my_flask_app "cd ~/audreychenportfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
