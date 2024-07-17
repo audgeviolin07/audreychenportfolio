180f02513f1c64073f846098b76b545de7704fa6
Samuel Lee <leesamuel423@gmail.com>
2024-07-17 04:45:47 -0400

    fix edge case test

--- tests/test_app.py
+++ tests/test_app.py
@@ -77,7 +77,7 @@ class AppTestCase(unittest.TestCase):
     #     assert "Invalid content" in html
     #
     #     # POST request with malformed email
-    #     response = self.client.post("/api/timeline_post", data={"email": "not-an-email", "content": "Hello world, I'm John!"})
+    #     response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
     #     assert response.status_code == 400
     #     html = response.get_data(as_text = True)
     #     assert "Invalid email" in html
